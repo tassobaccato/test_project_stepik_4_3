b73fcf6711ab1a97541f302bbed87bab93fd2a8e
unknown <kasila2@rambler.ru>
2022-09-02 22:38:13 +0200

    add changes

--- pages/base_page.py
+++ pages/base_page.py
@@ -72,8 +72,3 @@ class BasePage(object):
     def go_to_valid_login_link(self):
         valid_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         valid_link.click()
-
-
-class MainPage(BasePage):    # В классе MainPage у нас не осталось никаких методов, поэтому добавим туда заглушку
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
---
+++ pages/main_page.py
@@ -0,0 +1,6 @@
+from .pages.base_page import BasePage
+
+
+class MainPage(BasePage):    # В классе MainPage у нас не осталось никаких методов, поэтому добавим туда заглушку
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
